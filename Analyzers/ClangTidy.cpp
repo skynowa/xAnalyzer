bad57d9571d15e0c508f18b3dfa3b6c9f5cbd94c
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-10-14 19:57:37 +0300

    ClangTidy.cpp - ClangTidy, impl

--- Analyzers/ClangTidy.cpp
+++ Analyzers/ClangTidy.cpp
@@ -27,45 +27,6 @@ ClangTidy::~ClangTidy()
 void_t
 ClangTidy::run() /* override */
 {
-#if 0
-def runClangTidy(self):
-	"" Run analysis by Clang-Tidy ""
-
-	line_filter = "" # n/a
-
-	args_stdlib = {
-		self.COMPILER_ID_CLANG: "-stdlib=libstdc++",
-		self.COMPILER_ID_GCC:   ""
-	}
-
-	force_cpp = "-x " + options.CPP_LANG
-
-	header_filter = ""
-	if (options.QUICK_CHECK == 1):
-		header_filter = "" # skip
-	else:
-		header_filter = "^{}/.*".format(self.PROJECT_DIR) # all
-
-	cmd = \
-		"clang-tidy " \
-		"{} " \
-		"-system-headers=0 " \
-		"-line-filter={} " \
-		"-header-filter={} " \
-		"-extra-arg=-std={} " \
-		"-extra-arg={} " \
-		"-quiet " \
-		"-- " \
-		"{} " \
-		"{}" \
-		.format(self._git_modified_files, line_filter, header_filter,
-			options.CPP_STD, args_stdlib[self._complier_id], self._include_dirs,
-			force_cpp) \
-		.split()
-	print("cmd: ", cmd)
-
-	return subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-#else
 	// TODO: complier_id - impl
 	const auto complierId = ::CompilerId::COMPILER_ID_UNKNOWN;
 
@@ -107,7 +68,6 @@ def runClangTidy(self):
 	std::tstring_t stdError;
 
 	Process::execute("clang-tidy", params, {}, xTIMEOUT_INFINITE, &stdOut, &stdError);
-#endif
 }
 //-------------------------------------------------------------------------------------------------
 void_t
