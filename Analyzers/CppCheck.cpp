667d483e69163699246871e4e179eea0648e21c8
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-10-14 19:33:58 +0300

    CppCheck.cpp - CppCheck, impl

--- Analyzers/CppCheck.cpp
+++ Analyzers/CppCheck.cpp
@@ -50,7 +50,28 @@ def runCppcheck(self):
 
 	return subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
 #else
+	std::ctstring_t include_dirs       = "self._include_dirs";
+	std::ctstring_t git_modified_files = "self._git_modified_files";
 
+	std::cvec_tstring_t params
+	{
+		include_dirs,
+		git_modified_files,
+		"--library=std.cfg", "--library=posix.cfg",
+		/// "-UKERN_PROC_PATHNAME",
+		"--enable=" + ::CPPCHECK_ERROR_LEVEL, "--inconclusive",
+		"--language=c", "--language=" + ::CPP_LANG, "--std=" + ::CPP_STD,
+		"--platform=unix64",
+		"--force ",
+		"-j" + std::to_string(::JOBS_NUM),
+		"--relative-paths",
+		"--error-exitcode=1"
+	};
+
+	std::tstring_t stdOut;
+	std::tstring_t stdError;
+
+	Process::execute("cppcheck", params, {}, xTIMEOUT_INFINITE, &stdOut, &stdError);
 #endif
 }
 //-------------------------------------------------------------------------------------------------
--- Config.h
+++ Config.h
@@ -7,8 +7,10 @@
 #pragma once
 
 using namespace xl;
+
+#include <xLib/xLib.h>
 //-------------------------------------------------------------------------------------------------
-const std::ctstring_t appName = xT("xAnalyzer");
+std::ctstring_t appName = xT("xAnalyzer");
 //-------------------------------------------------------------------------------------------------
 #if 0
 """ Analyzer types """
@@ -50,6 +52,45 @@ CPP_MASK = {".h", ".hh", ".hpp", ".inl", ".cc", ".cpp", ".cxx"}
 "" Cppcheck ""
 CPPCHECK_ERROR_LEVEL = "warning,missingInclude"
 #else
+// Analyzer types
+// TYPE_CPPCHECK        = 1
+// TYPE_CLANG_TIDY      = 2
+// TYPE_CLANG_TIDY_DIFF = 3
+// TYPE_ACTIVE          = TYPE_CLANG_TIDY
+
+// Check mode
+// 0 - check changed files and headers
+// 1 - check only changed files
+
+// QUICK_CHECK = 1
+
+// Set OS environment variable to disable checks
+// From shell: export ANALYZER_SKIP_CHECK=1
+//
+// 0 - enable checks
+// 1 - skip checks
+
+// # SKIP_CHECK = os.environ.get("ANALYZER_SKIP_CHECK")
+// SKIP_CHECK = "1"
+
+// Disallow committing when errors/warnings occur
+// 0 - allow commit
+// 1 - disallow commit
+
+// STOP_ON_FAIL = 0
+
+// C++ language / standart
+std::ctstring_t CPP_LANG = "c++";
+std::ctstring_t CPP_STD  = "c++11";
+
+// Exclude files for checking
+// CPP_MASK = {".h", ".hh", ".hpp", ".inl", ".cc", ".cpp", ".cxx"}
+
+// Cppcheck
+std::ctstring_t CPPCHECK_ERROR_LEVEL = "warning,missingInclude";
+
+// Etc
+std::csize_t JOBS_NUM = 1;
 
 #endif
 //-------------------------------------------------------------------------------------------------
