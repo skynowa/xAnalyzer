0042ecd2b3994350bc86f4c25eab4ffb1ec961ef
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-11-02 13:19:24 +0200

    AnalyzerDataIn.cpp - add

---
+++ xAnalyzer.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+XLIB_INCLUDE="/usr/local/include:/home/skynowa/Projects/xLib/Include"
+
+export PATH=${XLIB_INCLUDE}:${PATH}
+export C_INCLUDE_PATH=${XLIB_INCLUDE}:${C_INCLUDE_PATH}
+export CPLUS_INCLUDE_PATH=${XLIB_INCLUDE}:${CPLUS_INCLUDE_PATH}
+
+echo "PATH: $PATH"
+
+/home/skynowa/Projects/xAnalyzer_eclipse/xAnalyzer
