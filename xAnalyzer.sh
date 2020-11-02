a38ca49ce05cac5b31fc2b2e37944ad2fd19f397
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-11-03 01:06:22 +0200

    xAnalyzer.sh - export envs, off

--- xAnalyzer.sh
+++ xAnalyzer.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 
-XLIB_INCLUDE="/usr/local/include:/home/skynowa/Projects/xLib/Include"
+# XLIB_INCLUDE="/usr/local/include:/home/skynowa/Projects/xLib/Include"
 
-export PATH=${XLIB_INCLUDE}:${PATH}
-export C_INCLUDE_PATH=${XLIB_INCLUDE}:${C_INCLUDE_PATH}
-export CPLUS_INCLUDE_PATH=${XLIB_INCLUDE}:${CPLUS_INCLUDE_PATH}
+# export PATH=${XLIB_INCLUDE}:${PATH}
+# export C_INCLUDE_PATH=${XLIB_INCLUDE}:${C_INCLUDE_PATH}
+# export CPLUS_INCLUDE_PATH=${XLIB_INCLUDE}:${CPLUS_INCLUDE_PATH}
 
-echo "PATH: $PATH"
+# echo "PATH: $PATH"
 
 /home/skynowa/Projects/xAnalyzer_eclipse/xAnalyzer
